36aa2c8262b2cf2fecd1f238b2c84693db87d56b
Lenilson Nascimento <lenilson.100@gmail.com>
2017-01-11 22:33:49 -0200

    Adding kubernetes

---
+++ kub_dev_controller_gen.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+cat > kub_dev_controller.json <<EOF
+{
+  "kind": "ReplicationController",
+  "apiVersion": "v1",
+  "metadata": {
+    "name": "graphql",
+    "labels": {
+      "name": "graphql"
+    }
+  },
+  "spec": {
+    "replicas": 1,
+    "selector": {
+      "name": "graphql"
+    },
+    "template": {
+      "metadata": {
+        "labels": {
+          "name": "graphql",
+          "deployment": "${WERCKER_GIT_COMMIT}"
+        }
+      },
+      "spec": {
+        "containers": [
+          {
+            "imagePullPolicy": "Always",
+            "image": "quay.io/lnlwd/graphql-playground:${WERCKER_GIT_COMMIT}",
+            "name": "cities",
+            "ports": [
+              {
+                "name": "http-server",
+                "containerPort": 3000,
+                "protocol": "TCP"
+              }
+            ]
+          }
+        ]
+      }
+    }
+  }
+}
+EOF
